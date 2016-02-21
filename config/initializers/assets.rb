27d8891dc8d2184be953e9533b04fe640b448807
Nick Touchette <touchette@gmail.com>
2016-02-21 03:26:28 -0500

    added more styling

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 
 Rails.application.config.assets.precompile += %w( onionbellylogo.png )
+Rails.application.config.assets.precompile += %w( onionbellylogovertical.png )
